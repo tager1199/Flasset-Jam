69d9a99e5cf56c096fc2b45b12946fd4b52e8701
tager1199 <17643144@students.lincoln.ac.uk>
2018-12-13 22:00:29 +0000

    Update

--- New Unity Project (2)/Assets/Birds.cs
+++ New Unity Project (2)/Assets/Birds.cs
@@ -1,17 +1,79 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Birds : MonoBehaviour {
-
+	int lives = 5;
+	bool IsUp = false;
+	float timer = 0;
 	// Use this for initialization
 	void Start () {
+
+	}
+
+
+	void Play()
+	{
 		
+		
+		Debug.Log("play");
+		Vector3 up = new Vector3(0, 2);
+		Vector3 down = new Vector3(0, -2);
+
+		float gametime = Time.time;
+		//bool clicked = false;
+		if (IsUp == false)
+		{
+			transform.Translate(up);
+			IsUp = true;
+		}
+
+		int WaitTime = 3;
+
+		if (gametime < 60 && gametime >20)
+		{
+			WaitTime = 2;
+		}
+		else if (gametime > 60)
+		{
+			WaitTime = 1;
+		}
+
+		if (timer >= WaitTime && IsUp == true)
+		{
+			Debug.Log("timerMax reached !");
+			transform.Translate(down);
+			IsUp = false;
+			timer = 0;
+
+		}
+
+		//transform.Translate(down);
+		//if (clicked == false)
+		//{
+		//	lives -= 1;
+		//}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Random.Range(1,10);
+
+		if (lives > 0)
+		{
+			
+			int rand = UnityEngine.Random.Range(1, 1000);
+			if (rand < 5)
+			{
+				Play();
+				lives = 0;
+			}
+								
+		}
+		timer += Time.deltaTime;
+		
+
+
 	}
 }
