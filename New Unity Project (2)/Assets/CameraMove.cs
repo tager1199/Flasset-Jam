98215d3951193384ebdcdbdd9c7f413b625a7886
tager1199 <17643144@students.lincoln.ac.uk>
2018-12-13 19:27:17 +0000

    added assets to scene and camera script
    added all the assets into the scene and added a script to camera allowing it to rotate

---
+++ New Unity Project (2)/Assets/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour
+{
+
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		int speed = 2;
+		Vector3 point = new Vector3 (3, 3, 3);
+
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.RotateAround(point, Vector3.down, speed);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.RotateAround(point, Vector3.up, speed);
+		}
+		
+	}
+}
